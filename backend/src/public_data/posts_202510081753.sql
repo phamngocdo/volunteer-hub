921d68c37f2e2317b7ba7193574d7f18ed33a646
quangduy164 <duyhx164@gmail.com>
2025-10-08 17:55:44 +0700

    update data

---
+++ backend/src/public_data/posts_202510081753.sql
@@ -0,0 +1,6 @@
+INSERT INTO public.posts (event_id,user_id,images_url,"content",created_at,updated_at) VALUES
+	 (1,1,'https://example.com/post1.jpg','Buổi dọn rác hôm nay thật ý nghĩa!','2025-10-08 17:47:06.264195+07','2025-10-08 17:47:06.264195+07'),
+	 (1,3,'https://example.com/post2.jpg','Mọi người rất nhiệt tình và vui vẻ.','2025-10-08 17:47:06.264195+07','2025-10-08 17:47:06.264195+07'),
+	 (3,5,'https://example.com/post3.jpg','Hiến máu cứu người – một hành động nhỏ, ý nghĩa lớn.','2025-10-08 17:47:06.264195+07','2025-10-08 17:47:06.264195+07'),
+	 (4,1,'https://example.com/post4.jpg','Chạy bộ 5km cùng bạn bè thật tuyệt!','2025-10-08 17:47:06.264195+07','2025-10-08 17:47:06.264195+07'),
+	 (2,3,'https://example.com/post5.jpg','Mong chờ ngày trồng cây sắp tới!','2025-10-08 17:47:06.264195+07','2025-10-08 17:47:06.264195+07');
